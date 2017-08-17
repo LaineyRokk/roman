1e9d3623553c7c32a7d3d39404ec8192141d047d
lainey rokkan and rebecca mcintosh <lainey@>
2017-08-17 11:52:38 -0700

    take numbers in multiples of 10s, 100s, 1000s and convert them roman numerals

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,27 @@ $(document).ready(function(){
   $("#form").submit(function(event) {
     event.preventDefault();
     var inputForm = parseInt($("#inputForm").val());
-    
-  })
-})
+    var answer = "";
+    for (var i=0; i<5; i++) {
+      if (inputForm >= 1000) {
+        inputForm = inputForm - 1000;
+        answer = answer.concat("M");
+        alert(answer);
+      }
+    }
+    for (var i=0; i<5; i++) {
+      if (inputForm >= 100) {
+        inputForm = inputForm - 100;
+        answer = answer.concat("C");
+        alert(answer);
+      }
+    }
+    for (var i=0; i<5; i++) {
+      if (inputForm >= 10) {
+        inputForm = inputForm - 10;
+        answer = answer.concat("X");
+        alert(answer);
+      }
+    }
+  });
+});
